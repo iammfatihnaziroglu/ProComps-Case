aed80db81032f308854d7492d0db7ddc44035806
Mehmet Fatih Nazıroğlu <m.fatihnaziroglu@gmail.com>
2024-06-14 14:32:22 +0300

    file image name changed/fixed to icons, and created the ListComponent1

--- src/app/components/CompsPage.tsx
+++ src/app/components/CompsPage.tsx
@@ -1,19 +1,11 @@
 import React from "react";
-import DeleteComp from "./modules/DeleteCompButton";
+import ListComponent1 from "./base-components/ListComponent1";
 
 const CompsPage: React.FC = () => {
   return (
     <div>
-      <DeleteComp />
+      <ListComponent1 />
     </div>
-    // <div className="bg-card_default-500 max-w-screen left-5 top-5 flex h-[632px] max-h-screen w-[1056px]">
-    //   <div></div>
-    //   <div className="ml-8 mt-12 flex h-[552px] w-[1008px] items-center justify-center">
-    //     <div className="relative h-full w-full overflow-hidden">
-    //       <div className="absolute right-0 top-1 flex h-[250px] w-[4px] overflow-y-auto rounded-full bg-scroll"></div>
-    //     </div>
-    //   </div>
-    // </div>
   );
 };
 
--- src/app/components/base-components/ListComponent1.tsx
+++ src/app/components/base-components/ListComponent1.tsx
@@ -2,7 +2,13 @@ import React from "react";
 
 function ListComponent1() {
   return (
-    <main className="min-w-screen flex min-h-screen items-center justify-center "></main>
+    <div className="flex h-20 w-[976px] items-center rounded-[200px]">
+      <div className="flex h-20 w-[976px] items-center justify-center rounded-[200px] border border-delete-border">
+        <div className="flex h-10 w-[896px] flex-row items-center justify-start">
+          a
+        </div>
+      </div>
+    </div>
   );
 }
 
